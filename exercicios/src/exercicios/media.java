070b53c963adea758c14b034e270740094b0eb46
Abner Cordeiro de Almeida <abnersolk@gmail.com>
2026-08-17 16:39:13 -0300

    continuando atividade-04

--- exercicios/src/exercicios/media.java
+++ exercicios/src/exercicios/media.java
@@ -1,18 +1,71 @@
 import java.util.*;
+
 public class media {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int m = input.nextInt();
-        int n = input.nextInt();
+        int maior = 0, menor = 0, uniao = 0;
+        int[] x, y, z;
 
-        int[] x, y;
+        System.out.print("Digite o número de elementos em X: ");
+        int m = input.nextInt();
         x = new int[m];
+
+        System.out.print("Digite o número de elementos em y: ");
+        int n = input.nextInt();
         y = new int[n];
 
+        System.out.print("Digite os " + m + " elementos de x: ");
         for (int i = 0; i < m; i++) {
-            
+            x[i] = input.nextInt();
         }
-}
+
+        System.out.print("Digite os " + n + " elementos de y: ");
+        for (int i = 0; i < m; i++) {
+            y[i] = input.nextInt();
+        }
+        if (m > n) {
+            maior = x.length;
+            menor = y.length;
+
+            for (int i = 0; i < maior; i++) {
+                for (int j = 0; j < menor; j++) {
+                    if (x[i] == y[j]) {
+                        uniao++;
+                    }
+                }
+            }
+
+            z = new int[uniao];
+            for (int i = 0; i < maior; i++) {
+                for (int j = 0; j < menor; j++) {
+                    if (x[i] == y[j]) {
+                        z[uniao] = x[i];
+                    }
+                }
+            }
+
+        } else {
+            maior = y.length;
+            menor = x.length;
+
+            for (int i = 0; i < maior; i++) {
+                for (int j = 0; j < menor; j++) {
+                    if (y[i] == x[j]) {
+                        uniao++;
+                    }
+                }
+            }
+
+            z = new int[uniao];
+            for (int i = 0; i < maior; i++) {
+                for (int j = 0; j < menor; j++) {
+                    if (y[i] == x[j]) {
+                        z[uniao] = y[i];
+                    }
+                }
+            }
+        }
+    }
 }
