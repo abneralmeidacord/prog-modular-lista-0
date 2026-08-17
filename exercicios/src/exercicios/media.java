a1d85fd69bc18c4cb965c43f8d8eebb4d713aa84
Abner Cordeiro de Almeida <abnersolk@gmail.com>
2026-08-17 12:18:09 -0300

    comeco atividade-04

--- exercicios/src/exercicios/media.java
+++ exercicios/src/exercicios/media.java
@@ -1,39 +1,18 @@
-package abnerproject1;
-
 import java.util.*;
-
 public class media {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        double nt1, nt2, nt3, soma, media, freq;
+        int m = input.nextInt();
+        int n = input.nextInt();
 
-        System.out.println("Digite as suas três notas");
-        System.out.print("Digite a nota 1: ");
-        nt1 = input.nextDouble();
-        System.out.print("Digite a nota 2: ");
-        nt2 = input.nextDouble();
-        System.out.print("Digite a nota 3: ");
-        nt3 = input.nextDouble();
+        int[] x, y;
+        x = new int[m];
+        y = new int[n];
 
-        soma = nt1 + nt2 + nt3;
-
-        media = soma / 3;
-
-        System.out.println("Sua média é: " + media);
-
-        System.out.print("Digite a sua frequencia (em decimal | Ex: 0.75):");
-        freq = input.nextDouble();
-
-        if (media >= 7.0 && freq >= 0.75) {
-            System.out.print("Aprovado! :)");
-        } else if (4.0 <= media && freq >= 0.75 || media < 7.0 && freq >= 0.75) {
-            System.out.print("Em recuperação! ;O;");
-        } else {
-            System.out.print("REPROVADO! !!!YOU LOSER!!!");
+        for (int i = 0; i < m; i++) {
+            
         }
-
-    };
-
+}
 }
